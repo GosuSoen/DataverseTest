bdbee82885b7ddc22f7060eceef138cbef40492b
gosu soen <gosusoen@outlook.com>
2019-11-04 11:58:47 +0200

    apply controls for delete / datatable configuration

--- DataverseTest/wwwroot/js/site.js
+++ DataverseTest/wwwroot/js/site.js
@@ -1,4 +1,35 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+//Turn contact table into datatable
+$(document).ready(function () {
+    $('#contactTable').DataTable();
+});
 
-// Write your JavaScript code.
+//Turn phones table into Datatable
+$(document).ready(function () {
+    $('#phonesTable').DataTable();
+});
+
+//Popup to ask user before contact delete
+
+confirmDelete = function () {
+    Swal.fire({
+        title: 'Are you sure you want to delete Contact?',
+        text: "You won't be able to revert this!",
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.value) {
+            return true;
+            Swal.fire(
+                'Deleted!',
+                'Contact has been deleted.',
+                'success'
+            )
+        }
+        return false;
+    })
+}
+
